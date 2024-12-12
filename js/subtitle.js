3fddae757ab7f8322fce67f04d32896725d33f85
gxhub <4733481357@stu.gcsnc.com>
2024-12-11 21:39:17 -0500

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -1,15 +1,15 @@
 function getRandomGreeting() {
   var greetings = [
-    "Beep boop! Are you a robot?",
-    "Did you know your weird!?",
+    "Beep boop! your not sigma, like gautham, you robot.",
+    "You unsigma tin can, unlike Gautham who is sigma and not a tin can :3",
     "How many sides does a circle have?",
     "How sigma are you",
     "Gautham is AWESOME isn't he",
     "GAUTHAM IS AMAZING isn't he",
-    "That proves you are unusual",
-    "What's my age again?",
-    "Sigma sigma on the wall, ____",
-    "I spent too much time making these",
+    "According to my book, your not sigma and Gautham is.",
+    "Wyd? Gautham made this so click games and have fun?",
+    "Sigma sigma on the wall, Gautham is the sigmaest of them ____",
+    "Gautham's websites are awesome obvi",
   ];
   var randomIndex = Math.floor(Math.random() * greetings.length);
   return greetings[randomIndex];
